002e3374930978b4a1704cbb4d9e380170f7c086
omuga <Obriel.muga.14@sansano.usm.cl>
2019-08-26 15:00:54 -0400

    Password Django

--- userdomain/user/models.py
+++ userdomain/user/models.py
@@ -2,8 +2,7 @@ from django.db import models
 
 class User(models.Model):
     username = models.CharField(max_length= 50)
-    password = models.CharField(max_length = 50)
-
+    password = forms.CharField(max_length=32, widget=forms.PasswordInput) 
     def __str__(self):
         return self.username
 
